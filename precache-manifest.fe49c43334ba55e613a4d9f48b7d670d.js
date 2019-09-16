4af0404a3b085080a87a8282458253017ea2884e
Osama Yousafzai <osama.yousafzai@gmai.com>
2019-09-16 10:05:31 +0300

    Updates

---
+++ precache-manifest.fe49c43334ba55e613a4d9f48b7d670d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb82f39d002fcfb114539594ec234a42",
+    "url": "/blog-redux/index.html"
+  },
+  {
+    "revision": "44868f0e8a8b8d4356db",
+    "url": "/blog-redux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d3ac177d726716dbb75a",
+    "url": "/blog-redux/static/js/2.be8adc1d.chunk.js"
+  },
+  {
+    "revision": "44868f0e8a8b8d4356db",
+    "url": "/blog-redux/static/js/main.af01baf0.chunk.js"
+  },
+  {
+    "revision": "d7933ee64d3b903adacc",
+    "url": "/blog-redux/static/js/runtime~main.19f4e744.js"
+  }
+]);
